ed616c578dfa1954975da387977c1ef6b70c5879
Skyler Cowley <skyler.cowley@gmail.com>
2014-04-18 19:56:08 -0600

    201404181955

--- src/Board.java
+++ src/Board.java
@@ -8,58 +8,51 @@ import java.util.List;
 /**
  * Created by Skyler on 4/16/14.
  */
-public class Board extends JPanel implements MouseListener{
+public class Board extends JPanel implements MouseListener {
     private final int SQRSIZE = 80;
-    private Checker [] [] board = new Checker [8] [8];
-    private List <List<Checker>> pieces = new ArrayList<List<Checker>>();
+    //private Checker[][] board = new Checker[8][8];
+    private List<List<Checker>> pieces = new ArrayList<List<Checker>>();
     private Point choice1 = new Point(-1, -1);
     private Point choice2 = new Point(-1, -1);
+    private Color player1 = Color.BLUE, player2 = Color.GREEN, turn = player1;
+    //private int turn = 1;
 
-    public Board(){
+    public Board() {
         setPreferredSize(new Dimension(640, 640));
         addMouseListener(this);
         //initalizing 2d array
-        for(int i = 0; i<8;i++){
+        for (int i = 0; i < 8; i++) {
             List<Checker> newList = new ArrayList<Checker>();
             pieces.add(i, newList);
-            for(int j = 0; j<8; j++){
-                Checker newCheck = new Checker(false);
+            for (int j = 0; j < 8; j++) {
+                Checker newCheck = new Checker(j, i, false);
                 pieces.get(i).add(j, newCheck);
             }
         }
 
         //checkers
-        for(int row = 0; row < 1; row++)
-            for(int col = 0; col < 8; col+=2){
-                //board[row][col].setColor(Color.BLUE);
-                //board[row][col].drawChecker(col, row, g);
-                Checker newCheck = new Checker(col, row, SQRSIZE, Color.BLUE);
-                pieces.get(col).add(row, newCheck);
+        for (int row = 0; row < 1; row++)
+            for (int col = 0; col < 8; col += 2) {
+                Checker newCheck = new Checker(col, row, SQRSIZE, player1);
+                pieces.get(col).set(row, newCheck);
             }
 
-        for(int row = 1; row < 2; row++)
-            for(int col = 1; col < 8; col+=2){
-                //board[row][col].setColor(Color.BLUE);
-                //board[row][col].drawChecker(col, row, g);
-                Checker newCheck = new Checker(col, row, SQRSIZE, Color.BLUE);
-                pieces.get(col).add(row, newCheck);
+        for (int row = 1; row < 2; row++)
+            for (int col = 1; col < 8; col += 2) {
+                Checker newCheck = new Checker(col, row, SQRSIZE, player1);
+                pieces.get(col).set(row, newCheck);
             }
 
         //checkers
-        for(int row = 6; row < 7; row++)
-            for(int col = 0; col < 8; col+=2){
-                //board[row][col].setColor(Color.GREEN);
-                //board[row][col].drawChecker(col, row, g);
-                Checker newCheck = new Checker(col, row, SQRSIZE, Color.GREEN);
-                pieces.get(col).add(row, newCheck);
+        for (int row = 6; row < 7; row++)
+            for (int col = 0; col < 8; col += 2) {
+                Checker newCheck = new Checker(col, row, SQRSIZE, player2);
+                pieces.get(col).set(row, newCheck);
             }
-        for(int row = 7; row < 8; row++)
-            for(int col = 1; col < 8; col+=2){
-                //board[row][col].setColor(Color.GREEN);
-                //board[row][col].drawChecker(col, row, g);
-
-                Checker newCheck = new Checker(col, row, SQRSIZE, Color.GREEN);
-                pieces.get(col).add(row, newCheck);
+        for (int row = 7; row < 8; row++)
+            for (int col = 1; col < 8; col += 2) {
+                Checker newCheck = new Checker(col, row, SQRSIZE, player2);
+                pieces.get(col).set(row, newCheck);
             }
 
     }
@@ -69,105 +62,92 @@ public class Board extends JPanel implements MouseListener{
 
         //black components
         g.setColor(Color.BLACK);
-
         //squares
-        for(int row = 0; row < 8; row+=2)
-            for(int col = 0; col < 8; col+=2)
+        for (int row = 0; row < 8; row += 2)
+            for (int col = 0; col < 8; col += 2)
                 g.fillRect(SQRSIZE * col, SQRSIZE * row, SQRSIZE, SQRSIZE);
-        for(int row = 1; row < 8; row+=2)
-            for(int col = 1; col < 8; col+=2)
+        for (int row = 1; row < 8; row += 2)
+            for (int col = 1; col < 8; col += 2)
                 g.fillRect(SQRSIZE * col, SQRSIZE * row, SQRSIZE, SQRSIZE);
 
         g.setColor(Color.RED);
-
-        for(int row = 1; row < 8; row+=2)
-            for(int col = 0; col < 8; col+=2)
+        for (int row = 1; row < 8; row += 2)
+            for (int col = 0; col < 8; col += 2)
                 g.fillRect(SQRSIZE * col, SQRSIZE * row, SQRSIZE, SQRSIZE);
 
-        for(int row = 0; row < 8; row+=2)
-            for(int col = 1; col < 8; col+=2)
+        for (int row = 0; row < 8; row += 2)
+            for (int col = 1; col < 8; col += 2)
                 g.fillRect(SQRSIZE * col, SQRSIZE * row, SQRSIZE, SQRSIZE);
 
-        //for(int i = 0; i < 8; i++)
-            //for(int j = 0; j <8; j++)
-                //board[i][j] = new Checker();
-
-        /////////////////////////
-
-        //System.out.println("test");
-
-        for(int i = 0; i<8;i++){
-            //List<Checker> newList = new ArrayList<Checker>();
-            //pieces.get(i);
-            //System.out.println("test2");
-            for(int j = 0; j<8; j++){
-                //System.out.println("test3");
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
                 pieces.get(i).get(j).drawChecker(g);
             }
         }
-
-
-
     }
 
-    public static void main(String [] args) {
+    public void move() {
+        Checker newCheck = new Checker(choice2, SQRSIZE, pieces.get(choice1.x).get(choice1.y).getColor());
+        pieces.get(choice2.x).set(choice2.y, newCheck);
+        Checker newCheck2 = new Checker(choice1.x, choice1.y, false);
+        pieces.get(choice1.x).set(choice1.y, newCheck2);
+    }
+
+    public static void main(String[] args) {
 
         JFrame frame = new JFrame("Checkers");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.getContentPane().add(new Board());
         frame.pack();
         frame.setVisible(true);
-        //frame.setResizable(false);
+        frame.setResizable(false);
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        int xPos = (e.getX()/SQRSIZE);
-        int yPos = (e.getY()/SQRSIZE);
 
-        System.out.println(xPos +" "+ yPos);
+        int xPos = (e.getX() / SQRSIZE);
+        int yPos = (e.getY() / SQRSIZE);
 
-        if(choice1.x == -1){
-            System.out.println("testpoint3");
-            choice1 = new Point(xPos, yPos);
-            //choice1.x = xPos;
-            //choice1.y = yPos;
+        if (xPos < 8 && yPos < 8) {
 
-        } else if(choice1.x == xPos && choice1.y == yPos){
-            System.out.println("testpoint2");
-            choice1 = new Point(-1, -1);
-            //choice1.x = -1;
-            //choice1.y = -1;
-        } else{
-            choice2 = new Point(xPos, yPos);
-            //choice2.x = xPos;
-            //choice2.y = yPos;
+            System.out.println(xPos + " " + yPos);
 
+            if (choice1.x == -1) {
+                if (pieces.get(xPos).get(yPos).getPresence()) {
+                    if (pieces.get(xPos).get(yPos).getColor() == turn) {
+                        choice1 = new Point(xPos, yPos);
+                        pieces.get(choice1.x).get(choice1.y).setSelected(true);
+                    }
+                }
+            } else if (choice1.x == xPos && choice1.y == yPos) {
+                pieces.get(choice1.x).get(choice1.y).setSelected(false);
+                choice1 = new Point(-1, -1);
+            } else {
+                if (!pieces.get(xPos).get(yPos).getPresence()) {
+                    /*if (turn == player1) {
+                        if ( (xPos == choice1.x+1 || xPos == choice1.x-1) && yPos == choice1.y + 1 ) {
+                            choice2 = new Point(xPos, yPos);
+                            move();
+                            turn = (turn == player1) ? player2 : player1;
+                            choice1 = new Point(-1, -1);
+                            choice2 = new Point(-1, -1);
+                        }
+                    }
+                    if (turn == player2) {
+                        if (true) {*/
+                            choice2 = new Point(xPos, yPos);
+                            move();
+                            turn = (turn == player1) ? player2 : player1;
+                            choice1 = new Point(-1, -1);
+                            choice2 = new Point(-1, -1);
+                        //}
+                    //}
+                }
 
-//            Checker temp1 = pieces.get(choice1.x).get(choice1.y);
-//            Checker temp2 = pieces.get(choice2.x).get(choice2.y);
-//
-//            pieces.get(choice1.x).add(choice1.y, temp2);
-//            pieces.get(choice2.x).add(choice2.y, temp1);
-
-
-            //Checker newCheck = pieces.get(choice1.x).remove(choice1.y);
-
-            //pieces.get(choice2.x).add(choice2.y, newCheck);
-            Checker newCheck = new Checker(choice2, SQRSIZE, pieces.get(choice1.x).get(choice1.y).getColor());
-            //pieces.get(choice2.x).set(choice2.y, pieces.get(choice1.x).remove(choice1.y));
-            pieces.get(choice2.x).set(choice2.y, newCheck);
-            Checker newCheck2 = new Checker(false);
-            pieces.get(choice1.x).set(choice1.y, newCheck2);
-
-            System.out.println("testpoint1");
-            choice1.x = -1;
-            choice1.y = -1;
-            choice2.x = -1;
-            choice2.y = -1;
-
+            }
+            repaint();
         }
-        repaint();
 
     }
 
--- src/Checker.java
+++ src/Checker.java
@@ -8,53 +8,69 @@ public class Checker {
     private Color color;
     private int xPos, yPos;
     private int radius;
-    private boolean king, present;
+    private boolean king, present, selected = false;
 
-    public Checker(){
+    public Checker() {
 
     }
 
-    public Checker(boolean presIn){
-
+    public Checker(int inX, int inY, boolean presIn) {
+        //xPos = inX;
+        //yPos = inY;
+        color = null;
         present = presIn;
+
     }
 
-    public Checker(int inX, int inY, int size, Color colorIn){
+    public Checker(int inX, int inY, int size, Color colorIn) {
         xPos = inX;
         yPos = inY;
-        radius = (size -20)/2;
+        radius = (size - 20) / 2;
         color = colorIn;
+        present = true;
     }
 
-    public Checker(Color colorIn){
-        color = colorIn;
-
-    }
-
-    public Checker(Point choice, int SQRSIZE, Color colorIn){
+    public Checker(Point choice, int SQRSIZE, Color colorIn) {
         xPos = choice.x;
         yPos = choice.y;
-        radius = (SQRSIZE - 20)/2;
+        radius = (SQRSIZE - 20) / 2;
         color = colorIn;
+        present = true;
 
 
     }
 
     //public void setX(int xIn) { xPos = xIn; }
     //public void setY(int yIn) { yPos = yIn; }
-    public void setColor(Color colorIn) { color = colorIn; }
-    public Color getColor() { return color;}
+    public void setColor(Color colorIn) {
+        color = colorIn;
+    }
 
-    /*public void drawChecker(int col, int row, Graphics g){
-        g.setColor(color);
-        g.fillOval(80 * col +10, 80 * row +10, 60, 60);
+    public Color getColor() {
+        return color;
+    }
 
-    }*/
+    public void setSelected(boolean selectedIn) {
+        selected = selectedIn;
+    }
 
-    public void drawChecker(Graphics g){
-        g.setColor(color);
-        g.fillOval(80 * xPos +10, 80 * yPos +10, 60, 60);
+    public boolean getPresence() {
+        return present;
+    }
 
+    public void drawChecker(Graphics g) {
+        //if (!present) {
+            g.setColor(color);
+            g.fillOval(80 * xPos + 10, 80 * yPos + 10, 60, 60);
+            if (selected) {
+                g.setColor(Color.ORANGE);
+                g.drawOval(80 * xPos + 10, 80 * yPos + 10, 60, 60);
+                g.drawOval(80 * xPos + 11, 80 * yPos + 11, 58, 58);
+                g.drawOval(80 * xPos + 12, 80 * yPos + 12, 56, 56);
+                //king
+                //g.fillOval(80 * xPos + 25, 80 * yPos + 25, 30, 30);
+            //}
+        }
     }
 }
 
